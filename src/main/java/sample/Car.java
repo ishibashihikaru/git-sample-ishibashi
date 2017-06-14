4effe4185a4a73777f646f99a7d9b8bbd7af6a37
ishibashihikaru <hikaru.ishibashi@rakus.co.jp>
2017-06-14 11:56:08 +0900

    speedとgasのgeter,setter作成

--- src/main/java/sample/Car.java
+++ src/main/java/sample/Car.java
@@ -1,6 +1,26 @@
 package sample;
 
+
 public class Car {
+	
+	/** スピード */
+	private int speed;
+	/** ガソリン */
+	private int gas;
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getGas() {
+		return gas;
+	}
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
 	
 
 }
