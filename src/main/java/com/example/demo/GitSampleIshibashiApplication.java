0d76790c581e612184f6a035592a470d284c78b3
ishibashihikaru <hikaru.ishibashi@rakus.co.jp>
2017-06-14 10:28:39 +0900

    新規にプロジェクト作成

---
+++ src/main/java/com/example/demo/GitSampleIshibashiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleIshibashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleIshibashiApplication.class, args);
+	}
+}
